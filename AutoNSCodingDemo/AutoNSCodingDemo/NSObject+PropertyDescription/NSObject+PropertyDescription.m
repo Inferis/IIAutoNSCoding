192ea4f328c0b16795d68a3bcaa9539a45ef7612
Inferis <spam@inferis.org>
2015-05-28 17:46:18 +0200

    Better propertydescription.

--- AutoNSCodingDemo/AutoNSCodingDemo/NSObject+PropertyDescription/NSObject+PropertyDescription.m
+++ AutoNSCodingDemo/AutoNSCodingDemo/NSObject+PropertyDescription/NSObject+PropertyDescription.m
@@ -9,17 +9,12 @@
 
 #define isPrefix(class, prefix) ([NSStringFromClass(class) rangeOfString:prefix].location == 0)
 
+#define getValue(obj, selector, type) ({ type(*objc_msgSendTyped)(id, SEL) = (void *)objc_msgSend; objc_msgSendTyped(obj, selector); })
+
 @implementation NSObject (PropertyDescription)
 
 - (NSArray *)propertyDescriptionInternal:(Class)class usingTypes:(BOOL)usingTypes {
     if (!class) class = [self class];
-
-    if (isPrefix(class, @"NS") || isPrefix(class, @"CA") || isPrefix(class, @"UI") || isPrefix(class, @"CA")) {
-        return @[[self description]];
-    }
-
-    unsigned int outCount, i;
-    objc_property_t *properties = class_copyPropertyList(class, &outCount);
 
     if ([self isKindOfClass:[NSDictionary class]]) {
         NSMutableArray *result = [NSMutableArray arrayWithObject:@"("];
@@ -52,12 +47,15 @@
         [result addObject:@")"];
         return result;
     }
-    
+
+    unsigned int outCount, i;
+    objc_property_t *properties = class_copyPropertyList(class, &outCount);
+
     NSMutableArray *result = [NSMutableArray arrayWithObject:[NSString stringWithFormat:@"<%@:#%lu> {", [self class], (unsigned long)[self hash]]];
     while (YES) {
         for (i = 0; i < outCount; i++) { 
             objc_property_t property = properties[i];
-            NSString *aName = [NSString stringWithCString:property_getName(property) encoding:NSUTF8StringEncoding];
+            NSString *aName = [NSString stringWithUTF8String:property_getName(property)];
 
             if ([aName isEqualToString:@"description"] || [aName isEqualToString:@"debugDescription"]) {
                 continue;
@@ -68,27 +66,136 @@
             free(attrValue);
             
             attrValue = property_copyAttributeValue(property, "G");
-            NSString *getter = attrValue ? [NSString stringWithUTF8String:attrValue] : aName;
+            SEL getter = NSSelectorFromString(attrValue ? [NSString stringWithUTF8String:attrValue] : aName);
             free(attrValue);
-
+            
             NSArray *valueDescription = nil;
-            if ([type rangeOfString:@":"].location != NSNotFound) {
-                valueDescription = @[ @"SEL" ];
+            if (!getter || [self respondsToSelector:getter]) {
+                valueDescription = @[@"@optional"];
             }
-            else if ([self respondsToSelector:NSSelectorFromString(getter)]) {
-                id value = [self valueForKey:aName];
-                if ([value isKindOfClass:[UIView class]]) {
-                    valueDescription = @[ [value class] ];
-                }
-                else if ([value isKindOfClass:[CALayer class]]) {
-                    valueDescription = @[ [value class] ];
+
+            id value = nil;
+            BOOL foundValue = NO;
+            if ([type characterAtIndex:0] == '@' && type.length >= 3) {
+                NSString *className = [type substringWithRange:NSMakeRange(2, type.length-3)];
+                Class class = NSClassFromString(className);
+
+                if (class) {
+                    value = getValue(self, getter, id);
+                    foundValue = YES;
+                    valueDescription = [value propertyDescriptionInternal:nil usingTypes:usingTypes];
                 }
                 else {
-                    valueDescription = [value propertyDescriptionInternal:nil usingTypes:usingTypes];
+                    valueDescription = @[[NSString stringWithFormat:@"?(%@)", type]];
                 }
             }
             else {
-                valueDescription = @[ @"@optional" ];
+                foundValue = YES;
+                switch ([type characterAtIndex:0]) {
+                    case ':': { // selector
+                        value = [NSString stringWithFormat:@"@selector(%@)", NSStringFromSelector(getValue(self, getter, SEL))];
+                        break;
+                    }
+                        
+                    case 'i': { // int
+                        value = @(getValue(self, getter, int));
+                        break;
+                    }
+                        
+                    case 's': { // short
+                        value = @(getValue(self, getter, short));
+                        break;
+                    }
+                        
+                    case 'l': { // long
+                        value = @(getValue(self, getter, long));
+                        break;
+                    }
+                        
+                    case 'q': { // long long
+                        value = @(getValue(self, getter, long long));
+                        break;
+                    }
+                        
+                    case 'I': { // unsigned int
+                        value = @(getValue(self, getter, unsigned int));
+                        break;
+                    }
+                        
+                    case 'S': { // unsigned short
+                        value = @(getValue(self, getter, unsigned short));
+                        break;
+                    }
+                        
+                    case 'L': { // unsigned long
+                        value = @(getValue(self, getter, unsigned long));
+                        break;
+                    }
+                        
+                    case 'Q': { // unsigned long long
+                        value = @(getValue(self, getter, unsigned long long));
+                        break;
+                    }
+                        
+                    case 'f': { // float
+                        value = @(getValue(self, getter, float));
+                        break;
+                    }
+                        
+                    case 'd': { // double
+                        value = @(getValue(self, getter, double));
+                        break;
+                    }
+                        
+                    case 'B': { // BOOL
+                        value = @(getValue(self, getter, BOOL));
+                        break;
+                    }
+                        
+                    case 'c': { // char
+                        value = @(getValue(self, getter, char));
+                        break;
+                    }
+                        
+                    case 'C': { // unsigned char
+                        value = @(getValue(self, getter, unsigned char));
+                        break;
+                    }
+                        
+                    case '#': { // class
+                        value = [NSString stringWithFormat:@"@class(%@)", NSStringFromClass(getValue(self, getter, Class))];
+                        break;
+                    }
+
+                    case '{': { // struct
+                        value = getValue(self, getter, void*) ? [NSString stringWithFormat:@"@struct(%@)", type] : nil;
+                        break;
+                    }
+
+                    case '^': { // pointer
+                        void *ptr = getValue(self, getter, void*);
+                        value = ptr ? [NSString stringWithFormat:@"@pointer(%p)", ptr] : nil;
+                        break;
+                    }
+
+                    case '@': { // block
+                        id block = getValue(self, getter, id);
+                        value = block ? [NSString stringWithFormat:@"@block(%@)", block] : nil;
+                        break;
+                    }
+
+                    default: {
+                        foundValue = NO;
+                        break;
+                    }
+                }
+            }
+
+            if (foundValue) {
+                valueDescription = value ? [value propertyDescriptionInternal:nil usingTypes:usingTypes] : @[@"nil"];
+            }
+            else {
+                valueDescription = @[[NSString stringWithFormat:@"?(%@)", type]];
             }
 
             if (valueDescription.count > 0) {
@@ -100,7 +207,10 @@
         }
         
         class = [class superclass];
-        if (!class || [NSStringFromClass(class) rangeOfString:@"NS"].location == 0) break;
+        if (!class) { break; }
+
+        name = [[NSString alloc] initWithUTF8String:class_getImageName(class)];
+        if ([name rangeOfString:[[NSBundle mainBundle] bundlePath]].location == NSNotFound) { break; }
 
         free(properties);
         properties = class_copyPropertyList(class, &outCount);
